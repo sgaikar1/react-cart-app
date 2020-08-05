15746054527987ce48cba23a2ffdad1f52a365e8
Santosh Gaikar <santoshgaikar1@gmail.com>
2020-08-05 20:01:55 +0530

    Added redux to project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import "./css/style.css";
 import "./css/reset.css";
 import MenuCart from "./containers/MenuCart";
@@ -7,15 +7,22 @@ import { BrowserRouter, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import { connect } from "react-redux";
+import props from "prop-types"
+import { render } from "@testing-library/react";
 
-function App() {
+class App extends Component {
+  render(){
   return (
     <BrowserRouter>
       <div className="containers">
         <Navbar />
-        <Route exact path="/" component={ Home } />
-        <Route path="/About" component={ About } />
-        <Route path="/Contact" component={ Contact } />
+        <Route exact path="/" component={Home} />
+        <Route path="/About" component={About} />
+        <Route path="/Contact" component={Contact} />
+        <div>Age:<span>{this.props.age}</span></div>
+        <button onClick={this.props.onAgeUp}>Age Up</button>
+        <button onClick={this.props.onAgeDown}>Age Down</button>
         <div id="app-holder">
           <div >
             <MenuCart />
@@ -25,6 +32,21 @@ function App() {
     </BrowserRouter>
 
   );
-}
+  }
+};
 
-export default App;
+
+const mapStateToProps = (state) => {
+  return {
+    age: state.age
+  }
+};
+
+const mapDispachToProps = (dispach) => {
+  return {
+    onAgeUp: () => dispach({ type: 'AGE_UP' }),
+    onAgeDown: () => dispach({ type: 'AGE_DOWN' })
+  }
+};
+
+export default connect(mapStateToProps, mapDispachToProps)(App);
