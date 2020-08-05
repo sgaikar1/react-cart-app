c9bd945db024869cfdbf883a0cc6ec77a7c5c5f4
Santosh Gaikar <santoshgaikar1@gmail.com>
2020-08-05 17:46:09 +0530

    added react route support

--- src/App.js
+++ src/App.js
@@ -2,16 +2,28 @@ import React from "react";
 import "./css/style.css";
 import "./css/reset.css";
 import MenuCart from "./containers/MenuCart";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App() {
   return (
-    <div className="containers">
-      <div id="app-holder">
-    <div >
-      <MenuCart />
-    </div>
-    </div> 
-    </div>
+    <BrowserRouter>
+      <div className="containers">
+        <Navbar />
+        <Route exact path="/" component={ Home } />
+        <Route path="/About" component={ About } />
+        <Route path="/Contact" component={ Contact } />
+        <div id="app-holder">
+          <div >
+            <MenuCart />
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
+
   );
 }
 
